81a9002b1de431386f30ffc0f805ddae89778dab
camilovivas <47890754+camilovivas@users.noreply.github.com>
2019-08-09 18:26:24 -0500

    se empezaron las pruebas

---
+++ test/testBuscarminas.java
@@ -0,0 +1,20 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import modelo.Buscaminas;
+
+class testBuscarminas {
+
+	@Test
+	void test() {
+		
+	}
+	@Test
+	public void generarMinas(){
+		Buscaminas a = new Buscaminas(Buscaminas.PRINCIPIANTE);
+		
+		
+	}
+
+}
