180e3c802eb08f7dfccb8d91762e06b21c45f711
camilovivas <47890754+camilovivas@users.noreply.github.com>
2019-08-12 16:24:30 -0500

    se hacer arreglos

--- test/testBuscarminas.java
+++ test/testBuscarminas.java
@@ -32,6 +32,7 @@ class testBuscarminas {
 		String tablero = c.mostrarTablero();
 		String esperado = " 1 2 3 4 5 6 7 8";
 		//cual assert seria mejor para comparar los dos String
+		assertEquals(esperado, tablero);
 	}
 	
 	@Test
